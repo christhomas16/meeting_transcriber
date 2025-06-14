6afd2ac140e3bd366f4fccaca35a914c1b2764cf
Chris Thomas <44703269+christhomas16@users.noreply.github.com>
2025-06-13 21:47:07 -0400

    docs: Update run script

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 # Create a virtual environment
 python3.11 -m venv venv
 
 # Activate it
 source venv/bin/activate
-# On Windows, use: venv\Scripts\activate
 
 pip install -r requirements.txt
 
